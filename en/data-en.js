e0d1e4f34405c79f712eedc58cd587f431226bec
ussam-dri <oussama119driouich@gmail.com>
2024-06-01 13:00:27 +0100

    all

---
+++ en/data-en.js
@@ -0,0 +1,24 @@
+window.dataEn = {
+    logoDark: 'assets/img/logo.png',
+    logoWhite: 'assets/img/logo-2.svg',
+    navItems: [
+        { href: 'index.html', text: 'Home' },
+        { 
+            text: 'Services', 
+            submenu: [
+                { href: 'services/Digital-Marketing.html', text: 'Digital Marketing' },
+                { href: 'services/Web-Development.html', text: 'Web Development' },
+                { href: 'services/application-development.html', text: 'Application Development' }
+            ]
+        },
+        { href: 'service.html', text: 'About Us' },
+        { href: 'portfolio.html', text: 'Contact US' }
+    ],
+    location: 'Agadir<br />&nbsp; &nbsp; &nbsp; &nbsp; Morocco',
+    phoneNumber: '(+212) 658347462',
+    socialLinks: [
+        { href: 'https://www.facebook.com/', text: 'Facebook' },
+        { href: 'https://www.linkedin.com/', text: 'Linkedin' },
+        { href: 'https://www.instagram.com/', text: 'Instagram' }
+    ]
+};
---
+++ en/data-fr.js
@@ -0,0 +1,24 @@
+window.dataFr = {
+    logoDark: 'assets/img/logo.png',
+    logoWhite: 'assets/img/logo-2.svg',
+    navItems: [
+        { href: 'index.html', text: 'Accueil' },
+        { 
+            text: 'Services', 
+            submenu: [
+                { href: 'services/Digital-Marketing.html', text: 'Marketing Digital' },
+                { href: 'services/Web-Development.html', text: 'Développement Web' },
+                { href: 'services/application-development.html', text: 'Développement d\'Applications' }
+            ]
+        },
+        { href: 'service.html', text: 'À Propos de Nous' },
+        { href: 'portfolio.html', text: 'Contactez-Nous' }
+    ],
+    location: 'Agadir<br />&nbsp; &nbsp; &nbsp; &nbsp; Maroc',
+    phoneNumber: '(+212) 658347462',
+    socialLinks: [
+        { href: 'https://www.facebook.com/', text: 'Facebook' },
+        { href: 'https://www.linkedin.com/', text: 'Linkedin' },
+        { href: 'https://www.instagram.com/', text: 'Instagram' }
+    ]
+};
